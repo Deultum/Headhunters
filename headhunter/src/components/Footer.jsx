613d2202e280951c6884f43b395784b3174f9d51
Ivanov.I <ivanov.ivan.zhivkov@gmail.com>
2023-11-18 08:34:20 +0200

    navigation ,  body and footer added

---
+++ headhunter/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = ()=> {
+    return (
+<footer className="footer">
+  <div className="footer-content">
+    <p>&copy; 2023 Your Company. All rights reserved. <a href="#contact">Contact Us</a></p>
+  </div>
+</footer>
+    );
+};
+export default Footer;
---
+++ headhunter/src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+const Navigation  = () => {
+return (
+    <ul className="navbar">
+      <li className="logo">
+        <a href="#"><img src="company_logo.png" alt="Company Logo" /></a>
+      </li>
+      <li><a href="#about">About Us</a></li>
+      <li><a href="#login">Login</a></li>
+      <li><a href="#register">Register</a></li>
+      <li><a href="#catalog">Catalog</a></li>
+    </ul>
+)
+};
+export default Navigation ;
