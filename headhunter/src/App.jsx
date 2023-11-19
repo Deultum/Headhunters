7e888f982f1d7f44889025213c0a1597474dae8b
Ivanov.I <ivanov.ivan.zhivkov@gmail.com>
2023-11-19 13:01:36 +0200

    started implementation of routing

--- headhunter/src/App.jsx
+++ headhunter/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Navigation from './components/navigation'
 import Footer from './components/Footer'
 import Homepage from './components/Homepage';
--- headhunter/src/components/Navigation.jsx
+++ headhunter/src/components/Navigation.jsx
@@ -1,13 +1,14 @@
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 const Navigation  = () => {
 return (
     <ul className="navbar">
       <li className="logo">
         <a href="#"><img src="company_logo.png" alt="Company Logo" /></a>
       </li>
-      <li><a href="#about">About Us</a></li>
-      <li><a href="#login">Login</a></li>
-      <li><a href="#register">Register</a></li>
-      <li><a href="#catalog">Catalog</a></li>
+      <li><Link to="/about">About Us</a></li>
+      <li><Link to="/login">Login</a></li>
+      <li><Link to="/register">Register</a></li>
+      <li><Link to="/catalog">Catalog</a></li>
     </ul>
 )
 };
